06f01e5b7a5bb6294d80f817813c6d66df78b840
unknown <timothy.liyu.yao@gmail.com>
2019-01-12 23:08:24 -0500

    Added status and command support

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -12,7 +12,13 @@
           ripple
         >
           <v-list-tile-content>
-            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+            <v-list-tile-title>
+              <chat-message
+                :user="item.user"
+                :date="item.timestamp"
+                :text="item.text"
+                :type="item.type"/>
+            </v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-divider :key="index"></v-divider>
@@ -35,25 +41,16 @@
 </template>
 
 <script>
-
+import ChatMessage from './ChatMessage.vue';
 
 export default {
+  components: { ChatMessage },
   data: () => ({
     message: '',
     items: [
     ],
     websocket: null,
   }),
-
-  computed: {
-    serializeMessage() {
-      // TODO: Add more data to send.
-      return Buffer.from(JSON.stringify({
-        id: new Date().getTime(),
-        text: this.message,
-      }));
-    },
-  },
 
   mounted() {
     this.connectToWebsocket();
@@ -67,20 +64,53 @@ export default {
     connectToWebsocket() {
       const wsurl = `ws://${window.location.hostname}:8081/${this.$route.params.room}`;
       this.websocket = new WebSocket(wsurl);
-      this.websocket.onmessage = response => this.addMessage(response.data);
+      this.websocket.onmessage = this.handleMessage;
+    },
+    handleMessage(response) {
+      const data = this.deserialize(response.data);
+      if (data.type === 'message' || data.type === 'status') {
+        this.addMessage(data);
+      }
     },
     sendMessage() {
       if (!this.message) {
         return;
       }
-      this.websocket.send(this.serializeMessage);
+      this.websocket.send(this.serializeMessage(this.message));
       this.clearMessage();
     },
     clearMessage() {
       this.message = '';
     },
     addMessage(message) {
-      this.items.push({ id: new Date().getTime(), text: message });
+      this.items.push(
+        {
+          id: message.timestamp,
+          timestamp: new Date(message.timestamp),
+          user: message.user,
+          text: message.payload,
+          type: message.type,
+        },
+      );
+    },
+    deserialize(data) {
+      return JSON.parse(data);
+    },
+    serializeMessage(message) {
+      // TODO: Add more data to send.
+      const data = {
+        id: new Date().getTime(),
+        type: 'message',
+        payload: message,
+      };
+
+      const i = message.indexOf(' ');
+
+      if (message.startsWith('/') && i > -1) {
+        data.type = message.substring(1, i);
+        data.payload = message.substring(i + 1);
+      }
+      return Buffer.from(JSON.stringify(data));
     },
   },
 };
---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,35 @@
+<template>
+  <span>
+    <span v-if="type==='message'">
+      {{ user }} at {{ date | shortDate }} : {{ text }}
+    </span>
+    <span v-else>
+      <i>{{ user }} is {{ text }}</i>
+    </span>
+  </span>
+</template>
+
+<script>
+import dateFormat from 'dateformat';
+
+export default {
+  props: {
+    text: String,
+    date: Date,
+    user: String,
+    type: {
+      type: String,
+      default: 'message',
+    },
+  },
+  filters: {
+    shortDate(val) {
+      return dateFormat(val, 'h:MM TT');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
