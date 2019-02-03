35743e0791af4bcb1c17212365df4bb660959ffa
Tim Yao <timothy.liyu.yao@gmail.com>
2019-02-02 22:46:27 -0500

    Added cards A-10

--- src/components/PlayingCard.vue
+++ src/components/PlayingCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card" :class="{ flipped : flipped }" @click="flipped = !flipped">
     <div class="card-inner">
       <div class="face">
         <div class="grid" :class="color">
@@ -7,16 +7,30 @@
             <div>{{ rankView }}</div>
             <div class="corner-entity">{{ entity }}</div>
           </div>
-          <div class="center">
-            <div class="rank-A" v-if="rank==='A'">
-              <h1>{{ entity }}</h1>
+          <div class="grid-center center">
+            <div class="rank-2-8" v-if="'A2345678'.indexOf(rank) > -1">
+              <div
+                v-for="i in a15" :key="i"
+                class="center"
+                :class="{ rotate: pattern[rank].rotated.includes(i)}">
+                <template v-if="pattern[rank].indicies.includes(i)">
+                  {{ entity }}
+                </template>
+              </div>
             </div>
-            <div class="rank-2" v-else-if="rank==='2'">
-              <div class="flex xs12">{{ entity }}</div>
-              <div class="flex xs12 flip">{{ entity }}</div>
+            <div class="rank-9-T" v-else-if="'9T'.indexOf(rank) > -1">
+              <div
+                v-for="i in a21" :key="i"
+                class="center"
+                :class="{ rotate: pattern[rank].rotated.includes(i)}">
+                <template v-if="pattern[rank].indicies.includes(i)">
+                  {{ entity }}
+                </template>
+              </div>
             </div>
+            <div class="face-card" :class="faceClass" v-else></div>
           </div>
-          <div class="bottom flip">
+          <div class="bottom rotate">
             <div>{{ rankView }}</div>
             <div class="corner-entity">{{ entity }}</div>
           </div>
@@ -29,6 +43,16 @@
 </template>
 
 <script>
+const a15 = [];
+for (let i = 0; i < 15; i += 1) {
+  a15.push(i);
+}
+
+const a21 = [];
+for (let i = 0; i < 21; i += 1) {
+  a21.push(i);
+}
+
 export default {
   props: {
     rank: {
@@ -47,6 +71,54 @@ export default {
     },
   },
 
+  data: () => ({
+    flipped: false,
+    a15,
+    a21,
+    pattern: {
+      A: {
+        indicies: [7],
+        rotated: [],
+      },
+      2: {
+        indicies: [1, 13],
+        rotated: [13],
+      },
+      3: {
+        indicies: [1, 7, 13],
+        rotated: [13],
+      },
+      4: {
+        indicies: [0, 2, 12, 14],
+        rotated: [12, 14],
+      },
+      5: {
+        indicies: [0, 2, 7, 12, 14],
+        rotated: [12, 14],
+      },
+      6: {
+        indicies: [0, 2, 6, 8, 12, 14],
+        rotated: [12, 14],
+      },
+      7: {
+        indicies: [0, 2, 4, 6, 8, 12, 14],
+        rotated: [12, 14],
+      },
+      8: {
+        indicies: [0, 2, 4, 6, 8, 10, 12, 14],
+        rotated: [12, 14],
+      },
+      9: {
+        indicies: [0, 2, 6, 8, 12, 10, 14, 18, 20],
+        rotated: [12, 14, 18, 20],
+      },
+      T: {
+        indicies: [0, 2, 4, 6, 8, 12, 14, 16, 18, 20],
+        rotated: [12, 14, 16, 18, 20],
+      },
+    },
+  }),
+
   computed: {
     entity() {
       return {
@@ -64,11 +136,24 @@ export default {
     color() {
       return ['C', 'S'].includes(this.suit) ? 'black--text' : 'red--text';
     },
+
+    faceClass() {
+      return {
+        J: 'jack',
+        Q: 'queen',
+        K: 'king',
+      }[this.rank];
+    },
   },
 };
 </script>
 
 <style>
+
+.card {
+  cursor: pointer;
+  transition: all ease-in-out 0.05s;
+}
 
 .card, .grid {
   width: 63.5mm;
@@ -95,34 +180,40 @@ export default {
   grid-column: -1 / -2;
 }
 
-.flip {
+.rotate {
  transform: rotate(180deg);
 }
 
-.center {
+.grid-center {
   grid-row: 2 / -2;
   grid-column: 2 / -2;
-  font-size: 70px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.rank-A {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.rank-2 {
-  display: flex;
-  flex-wrap: wrap;
-  height: 100%;
-  align-content: space-between;
+  font-size: 60px;
 }
 
 .center {
-  /* border: 1px solid black; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.rank-2-8 {
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-rows: repeat(5, minmax(0, 1fr));
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+}
+
+.rank-9-T {
+  height: 93%;
+  width: 100%;
+  display: grid;
+  grid-template-rows: repeat(7, minmax(0, 1fr));
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+}
+
+.border {
+  border: 1px solid black;
 }
 
 .top, .bottom {
@@ -132,15 +223,13 @@ export default {
 
 .corner-entity {
   line-height: 0.7;
-  font-size: 40px;
+  font-size: 30px;
 }
 
-  /* The flip card container - set the width and height to whatever you want.
-  We have added the border property to demonstrate that the flip itself goes
-  out of the box on hover (remove perspective if you don't want the 3D effect */
 .card {
   background-color: transparent;
-  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+  perspective: 1000px;
+  transition: transform 0.3 ease-in-out;
 }
 
 /* This container is needed to position the front and back side */
@@ -149,14 +238,17 @@ export default {
   width: 100%;
   height: 100%;
   text-align: center;
-  transition: transform 0.3s;
+  transition: all 0.3s ease-in-out;
   transform-style: preserve-3d;
   border-radius: 10px;
 }
 
-/* Do an horizontal flip when you move the mouse over the flip box container */
-.card:hover .card-inner {
-  /* transform: rotateY(180deg); */
+.card.flipped .card-inner {
+  transform: rotateY(180deg);
+}
+
+.card:hover {
+  transform: translateY(-5%);
 }
 
 /* Position the front and back side */
@@ -169,12 +261,24 @@ export default {
 
 /* Style the back side */
 .back {
-  background-image: url("https://t3.rbxcdn.com/acab25608db99f3e6195c0ece3313b7f");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  color: white;
+  background:
+  linear-gradient(27deg, #151515 5px, transparent 5px) 0 5px,
+  linear-gradient(207deg, #151515 5px, transparent 5px) 10px 0px,
+  linear-gradient(27deg, #222 5px, transparent 5px) 0px 10px,
+  linear-gradient(207deg, #222 5px, transparent 5px) 10px 5px,
+  linear-gradient(90deg, #1b1b1b 10px, transparent 10px),
+  linear-gradient(#1d1d1d 25%, #1a1a1a 25%, #1a1a1a 50%,
+  transparent 50%, transparent 75%, #242424 75%, #242424);
+  background-color: #131313;
+  background-size: 20px 20px;
   transform: rotateY(180deg);
   border-radius: 10px;
+}
+
+.face-card {
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
 }
 </style>
