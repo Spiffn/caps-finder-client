f68611ddb9005edbb9f701a55662232d1866d10d
James Wu <therealjameswu@gmail.com>
2019-02-17 11:55:24 -0500

    Add timeline, using naive list modification LOL

--- src/components/GameTimeline.vue
+++ src/components/GameTimeline.vue
@@ -1,18 +1,18 @@
 <template>
   <v-timeline align-top dense>
     <v-timeline-item
-      color="red"
+      v-for="(item, index) in eventItems"
+      :key="index"
+      :color="item.color"
       icon="star"
       fill-dot
-      small
-    >
+      small>
       <v-layout pt-3>
         <v-flex xs3>
-          <strong>5pm</strong>
+          <strong>{{ item.timestamp | shortDate }}</strong>
         </v-flex>
       <v-flex>
-        <strong>New Icon</strong>
-        <div class="caption">Mobile App</div>
+        <strong>{{ item.payload }}</strong>
       </v-flex>
       </v-layout>
     </v-timeline-item>
@@ -20,8 +20,37 @@
 </template>
 
 <script>
-export default {
+import moment from 'moment';
+import _ from 'lodash';
 
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  computed: {
+    eventItems() {
+      const announcements = _.filter(this.items, item => item.type === 'announcement');
+      for (let i = 0; i < announcements.length; i += 1) {
+        if (i % 2 === 0) {
+          announcements[i].color = 'green';
+        } else {
+          announcements[i].color = 'purple';
+        }
+      }
+      console.log(announcements);
+      return announcements;
+    },
+  },
+
+  filters: {
+    shortDate(val) {
+      return moment(val).format('h:mm A');
+    },
+  },
 };
 </script>
 
